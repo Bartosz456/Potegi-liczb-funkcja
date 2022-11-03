c892552878dcfa1aa0d7ac0ab87bae3a1b8aafbb
Bartosz456 <bartoszsz14@gmail.com>
2022-11-03 19:33:30 +0100

    f

---
+++ potęgi.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int oblicznie_potegi(int podstawa, int wykladnik, int wynik = 1)
+{
+		for (size_t i = 0; i < wykladnik; i++)
+		{
+			wynik *= podstawa;
+		}
+		return wynik;
+}
+int main()
+{
+	int podstawa, wykladnik;
+	cout << "Podaj podstawe: ";
+	cin >> podstawa;
+	cout << "Podaj wykladnik: ";
+	cin >> wykladnik;
+	cout << oblicznie_potegi(podstawa, wykladnik);
+}
